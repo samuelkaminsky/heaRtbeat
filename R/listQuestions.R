e9207ea87ef7a80cbdf2292da5fa1fcfeb5309a1
jpurl <99049999+jpurl@users.noreply.github.com>
2022-02-06 10:52:46 -0800

    Create initial listQuestions.R

---
+++ R/listQuestions.R
@@ -0,0 +1,14 @@
+test <- data.frame(Questions = colnames(engsat.wide),
+                   Min.Value = apply(engsat.wide, 2,
+                                     FUN = function(x)
+                                       {min(x, na.rm = TRUE)}),
+                   Max.Value = apply(engsat.wide, 2,
+                                     FUN = function(x)
+                                     {max(x, na.rm = TRUE)}),
+                   Missingness = apply(engsat.wide, 2,
+                                     FUN = function(x)
+                                     {mean(is.na(x))}))
+                                     
+likert.items <- test[which(test$Min.Value > 0 &
+                            test$Max.Value < 6 &
+                            test$Max.Value > test$Min.Value), ]$Questions
