c659cac71c36165d60b00e5cb97c2651f29bac72
Samuel Kaminsky <23510580+samuelkaminsky@users.noreply.github.com>
2022-02-07 11:29:29 -0800

    Update and rename findVotes.R to find_votes.R

---
+++ R/find_votes.R
@@ -0,0 +1,21 @@
+heartbeat <- function(data, ..., threshold = 1) {
+  assertthat::assert_that(is.data.frame(data))
+  likert.cols <- dplyr::select(data, {{ likert.items }})
+  output <-
+    data %>%
+    dplyr::mutate(
+      individual.sd =
+        sqrt(
+          rowSums((likert.cols - rowMeans(likert.cols, na.rm = TRUE))^2,
+            na.rm = TRUE
+          ) / (rowSums(!is.na(likert.cols)) - 1)
+        ),
+      individual.mean = rowMeans(likert.cols, na.rm = TRUE),
+      dplyr::across(tidyselect::all_of(likert.items), ~
+      threshold < dplyr::if_else(
+        .data$individual.sd == 0, 0,
+        (.x - .data$individual.mean) / .data$individual.sd
+      ))
+    )
+  return(output)
+}
