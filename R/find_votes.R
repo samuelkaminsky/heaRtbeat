2f500387e36236ee6c801a1949e1f64518667a68
Sam Kaminsky <samuelkaminsky@google.com>
2022-02-09 16:05:59 -0800

    Update DESCRIPTION, turn on roxygen2, and comment out broken file

--- R/find_votes.R
+++ R/find_votes.R
@@ -1,6 +1,7 @@
+#' @export
 find_votes <- function(data, ..., threshold = 1) {
   assertthat::assert_that(is.data.frame(data))
-  likert.cols <- dplyr::select(data, {{ likert.items }})
+  likert.cols <- dplyr::select(data, {{ ... }})
   output <-
     data %>%
     dplyr::mutate(
@@ -11,7 +12,7 @@ find_votes <- function(data, ..., threshold = 1) {
           ) / (rowSums(!is.na(likert.cols)) - 1)
         ),
       individual.mean = rowMeans(likert.cols, na.rm = TRUE),
-      dplyr::across(tidyselect::all_of(likert.items), ~
+      dplyr::across(tidyselect::all_of(...), ~
       threshold < dplyr::if_else(
         .data$individual.sd == 0, 0,
         (.x - .data$individual.mean) / .data$individual.sd
