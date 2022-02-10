17623708489930ded44ec90ef1b3a62bc93aa29d
jpurl <99049999+jpurl@users.noreply.github.com>
2022-02-09 16:56:57 -0800

    Output upvote and downvotes

--- R/find_votes.R
+++ R/find_votes.R
@@ -1,6 +1,8 @@
 #' @export
 find_votes <- function(data, ..., threshold = 1) {
   assertthat::assert_that(is.data.frame(data))
+  assertthat::assert_that(threshold > 0,
+    msg = "Please a positive number of standard deviations above the mean to define an upvote.")
   likert.cols <- dplyr::select(data, {{ ... }})
   output <-
     data %>%
@@ -13,10 +15,20 @@ find_votes <- function(data, ..., threshold = 1) {
         ),
       individual.mean = rowMeans(likert.cols, na.rm = TRUE),
       dplyr::across(tidyselect::all_of(...), ~
-      threshold < dplyr::if_else(
-        .data$individual.sd == 0, 0,
-        (.x - .data$individual.mean) / .data$individual.sd
-      ))
+        case_when(
+          threshold <
+          dplyr::if_else(
+            .data$individual.sd == 0, 0,
+            (.x - .data$individual.mean) / .data$individual.sd
+          ) ~ 1,
+          -1*threshold >
+          dplyr::if_else(
+            .data$individual.sd == 0, 0,
+            (.x - .data$individual.mean) / .data$individual.sd
+          ) ~ -1,
+          TRUE ~ 0
+        )
+      )
     )
   return(output)
 }
